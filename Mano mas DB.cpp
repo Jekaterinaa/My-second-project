21bac4ee7785ab23ebe1837f728a4539abeea7ad
Jekaterinaa <71494864+Jekaterinaa@users.noreply.github.com>
2020-09-25 17:43:01 +0300

    Update Mano mas DB.cpp

--- Mano mas DB.cpp
+++ Mano mas DB.cpp
@@ -14,6 +14,7 @@ const int max_size = 10;
 struct duomuo {
     string vard, pav;
     float vid = 0, gp, med, paz[max_size], egz;
+    int num;
 };
 
 void insertionSort(int mas[], int n) {
@@ -41,11 +42,13 @@ float mediana(int mas[], int n) {
 
 int main()
 {
-    int k, paz, l, stud;
+    int k, l, stud, n;
+    float paz;
     cout << "Iveskite studentu skaiciu\n";
     cin >> stud;
     duomuo* eil_mas = new duomuo[stud];
     for (int j = 0; j < stud; j++) {
+        n = 0;
         cout << "Iveskite studento duomenis (vardas, pavarde):\n";
         cin >> eil_mas[j].vard >> eil_mas[j].pav;
         cout << "Iveskite 0, jeigu norite ivesti pazymius (ir 0, kai baigete ivedineti pazymius). 1 - jeigu norite, kad jie butu atsitiktinai generuojami.\n";
@@ -56,17 +59,17 @@ int main()
                 cin >> paz;
                 if (paz == 0)
                     break;
-                eil_mas[j].paz[num] = paz;
-                eil_mas[j].vid = eil_mas[j].vid + (float)paz;
+                eil_mas[j].paz[n] = paz;
+                eil_mas[j].vid = eil_mas[j].vid + paz;
                 num++;
             } while (num < max_size);
         }
         else if (l == 1) {
             eil_mas[j].egz = (rand() % 10) + 5;
-            eil_mas[j].num = rand() % 10 + 1;
+            eil_mas[j].num = (rand() % 10) + 1;
             for (int i = 0; i < eil_mas[j].num; i++) {
                 eil_mas[j].paz[i] = (rand() % 10) + 1;
-                eil_mas[j].vid = eil_mas[j].vid + (float)eil_mas[j].paz[i];
+                eil_mas[j].vid = eil_mas[j].vid + eil_mas[j].paz[i];
             }
         }
         else {
@@ -80,7 +83,7 @@ int main()
     if (k == 0) {
         cout << "Vardas      " << "Pavarde       " << "Galutinis(vid.)" << endl;
         cout << "-------------------------------------------------" << endl;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < stud; i++) {
             eil_mas[i].vid = eil_mas[i].vid / (float)eil_mas[i].num;
             eil_mas[i].gp = eil_mas[i].vid * 0.4 + eil_mas[i].egz * 0.6;
             cout << std::left << std::setw(12) << eil_mas[i].vard << std::left << std::setw(14) << eil_mas[i].pav;
@@ -89,8 +92,8 @@ int main()
     }
     else if (k == 1) {
         cout << "Vardas      " << "Pavarde       " << "Galutinis(med.)" << endl;
-        cout << "-----------------------------" << endl;
-        for (int i = 0; i < 5; i++) {
+        cout << "------------------------------------------------" << endl;
+        for (int i = 0; i < stud; i++) {
             insertionSort(eil_mas[i].paz, eil_mas[i].num);
             eil_mas[i].med = mediana(eil_mas[i].paz, eil_mas[i].num);
             eil_mas[i].gp = eil_mas[i].med * 0.4 + eil_mas[i].egz * 0.6;
