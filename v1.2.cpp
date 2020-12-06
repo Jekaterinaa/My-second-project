15630747b87912157c5db89bf2043297200edefc
Jekaterinaa <71494864+Jekaterinaa@users.noreply.github.com>
2020-12-06 17:11:03 +0200

    Add files via upload

---
+++ v1.2.cpp
@@ -0,0 +1,19 @@
+#include "studentas.h"
+
+int main()
+{
+    vector<duomuo> studentai, vargsiukai;
+    skaicius sk;
+    int kiek_paz, kiek;
+    int gp;
+
+    sk = duomenuGeneravimas();
+    kiek = sk.kiek;
+    kiek_paz = sk.kiek_paz;
+
+    studentai = duomenuNuskaitymas(kiek, kiek_paz);
+    duomenuDalijimas(studentai, vargsiukai, kiek);
+
+    isvedimasIFaila(studentai, kiek);
+    isvedimasIFaila(vargsiukai, kiek);
+}
